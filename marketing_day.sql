94ffc6b660da4e39907814a71c60c1918a7b8a53
zechwn <23FTT1117@student.pb.edu.bn>
2025-03-05 14:52:00 +0800

    Add files via upload

---
+++ marketing_day.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 05, 2025 at 07:51 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `marketing_day`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applications`
+--
+
+CREATE TABLE `applications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `job_id` int(11) NOT NULL,
+  `applied_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cvs`
+--
+
+CREATE TABLE `cvs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cv_file` varchar(255) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` enum('Male','Female','Other') NOT NULL,
+  `cgpa` decimal(3,2) NOT NULL,
+  `school` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `name`, `age`, `gender`, `cgpa`, `school`) VALUES
+('23FTT2934', 'John Doe', 25, 'Male', 2.30, 'School of Business');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_type` enum('graduate','company') NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `profile_pic` varchar(255) DEFAULT NULL,
+  `cv` varchar(255) DEFAULT NULL,
+  `ic_number` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `user_type`, `phone`, `profile_pic`, `cv`, `ic_number`) VALUES
+(1, 'John Doe', 'johndoe@example.com', 'adminpassword', '2025-01-27 07:58:44', '', '+673 1234 5678', 'profile_1.png', NULL, ''),
+(2, 'Group10', 'Group10@pb.edu.bn', '$2y$10$DQGo0hEPuXB37BCKYbB7nOzKQxHbsqq3eXxDnB/Xurj9rLVC5/FLi', '2025-01-27 07:59:21', 'graduate', '123123412', 'profile_2.png', NULL, ''),
+(3, 'Group10Company', 'Group10Company@pb.edu.bn', '$2y$10$nV1URJ5abNjrF0.wK1TARO05UxyVzNr76bWaEt1/Ksduk0HTMVaCu', '2025-01-27 07:59:43', 'company', NULL, NULL, NULL, ''),
+(4, 'Abdul Azim Bin Kahar', 'abdulazim321@gmail.com', '$2y$10$vZ1KrA0cwXf4JyB1wuWZtedULQc1xqdqJLRXAepOXHo97U3SYS3dO', '2025-02-27 07:36:52', 'graduate', '+673 7273841', 'profile_4.jpg', NULL, '01-124231'),
+(5, 'JohnCompany', 'JohnCompany@yahoo.com', '$2y$10$Y2Ew5fnL/BH401J8v5JjAO5GLKq7lNvSqFlQ3RpOrrYlUX9jPjY0K', '2025-02-27 08:39:55', 'company', NULL, NULL, NULL, ''),
+(6, 'Abul Muhammad', 'AbulMuhammad@gmail.com', '$2y$10$k4CKOm09ZftAlGI8Tr1vaOwzVO26vZhAErla/mDQsF.WQ9AyX6QBO', '2025-03-04 07:57:59', 'graduate', NULL, NULL, NULL, '01-123432'),
+(10, 'NumberOne', 'numberone@gmail.com', '$2y$10$WeBsd8U/89J2/7gponPlWu2c.HWH9QVSthieCK/ZJL8JgdKVMaroe', '2025-03-05 05:47:56', 'graduate', '11111111', 'profile_10.png', 'cv_10_01-111111.pdf', '01-111111'),
+(12, 'NumberTwo', '01-222222@graduate.pbu.edu.bn', '$2y$10$K0oRuZtXcmPWVgwv3L/OzOLhIj0PFH32EzpV2.cOUeYJLvWvR2Z3a', '2025-03-05 06:04:50', 'graduate', NULL, NULL, NULL, '01-222222'),
+(13, 'NumberThree', '01-333333@graduate.pbu.edu.bn', '$2y$10$/robmrYbIaZ3oUaURshtZuOln5UpUMYCi9bkYAMa2g01AaUNLOHLW', '2025-03-05 06:05:49', 'graduate', NULL, NULL, NULL, '01-333333');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `applications`
+--
+ALTER TABLE `applications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `job_id` (`job_id`);
+
+--
+-- Indexes for table `cvs`
+--
+ALTER TABLE `cvs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `applications`
+--
+ALTER TABLE `applications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cvs`
+--
+ALTER TABLE `cvs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `applications`
+--
+ALTER TABLE `applications`
+  ADD CONSTRAINT `applications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `applications_ibfk_2` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`);
+
+--
+-- Constraints for table `cvs`
+--
+ALTER TABLE `cvs`
+  ADD CONSTRAINT `cvs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
